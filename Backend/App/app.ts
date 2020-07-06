6ae383109da07efaf79905b23a4e2729a029465c
subhrapaladhi <subhrapaladhi9@gmail.com>
2020-07-06 20:53:52 +0530

    payment backend code

--- Backend/App/app.ts
+++ Backend/App/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
-import stripe from "stripe";
+import Stripe from "stripe";
 import {v4 as uuid} from "uuid";
 
 const app: express.Application = express();
+const stripe = new Stripe("sk_test_51H1uk0BtWMWqSvhsTRvmM8BhXv2VzpWdCrdIlUlG1KQPgdzL1MkQBrPMqXx8pLHooWyKS1LksNsPtOVU6vZF9ltC00SdqycDNs",{
+    apiVersion: "2020-03-02"
+})
+
 
 // MIDDLEWARES
 app.use(express.json());
@@ -13,6 +17,40 @@ app.use(cors());
 // ROUTES
 app.get("/", (req, res) => {
     res.send("Its working");
+})
+
+app.post("/payment", (req,res) => {
+    const {product, token} = req.body;
+    console.log("product = ", product);
+    console.log("price = ", product.price );
+    
+    const idempotencyKey = uuid();
+
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    })
+    .then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'usd',
+            customer: customer.id,
+            receipt_email: <string>customer.email,
+            description: product.name,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    country: token.card.address_coutry,
+                    line1: token.card.address_line1
+                }
+            }
+        },{idempotencyKey})
+    })
+    .then(result => res.status(200).json(result))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({err});
+    })
 })
 
 // START SERVER
--- Backend/Build/app.js
+++ Backend/Build/app.js
@@ -5,13 +5,49 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const stripe_1 = __importDefault(require("stripe"));
+const uuid_1 = require("uuid");
 const app = express_1.default();
+const stripe = new stripe_1.default("sk_test_51H1uk0BtWMWqSvhsTRvmM8BhXv2VzpWdCrdIlUlG1KQPgdzL1MkQBrPMqXx8pLHooWyKS1LksNsPtOVU6vZF9ltC00SdqycDNs", {
+    apiVersion: "2020-03-02"
+});
 // MIDDLEWARES
 app.use(express_1.default.json());
 app.use(cors_1.default());
 // ROUTES
 app.get("/", (req, res) => {
     res.send("Its working");
+});
+app.post("/payment", (req, res) => {
+    const { product, token } = req.body;
+    console.log("product = ", product);
+    console.log("price = ", product.price);
+    const idempotencyKey = uuid_1.v4();
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    })
+        .then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'usd',
+            customer: customer.id,
+            receipt_email: customer.email,
+            description: product.name,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    country: token.card.address_coutry,
+                    line1: token.card.address_line1
+                }
+            }
+        }, { idempotencyKey });
+    })
+        .then(result => res.status(200).json(result))
+        .catch(err => {
+        console.log(err);
+        res.status(500).json({ err });
+    });
 });
 // START SERVER
 const PORT = process.env.PORT || 3000;
